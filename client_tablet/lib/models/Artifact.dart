68556096d914cb2c26256dcda4ea97ef846f41c8
Nourhan Mohamed <mnourhan4526@gmail.com>
2023-03-21 16:38:26 +0200

    commit 20

---
+++ client_tablet/lib/models/Artifact.dart
@@ -0,0 +1,24 @@
+class Artifact {
+  final String name;
+  final String museum;
+  final String description;
+  final String image;
+  final String video;
+
+  const Artifact(
+      {required this.name,
+      required this.museum,
+      required this.description,
+      required this.image,
+      required this.video});
+
+  factory Artifact.fromJson(Map<String, dynamic> json) {
+    return Artifact(
+      name: json['name'],
+      museum: json['museum'],
+      description: json['description'],
+      image: json['image'],
+      video: json['video'],
+    );
+  }
+}
