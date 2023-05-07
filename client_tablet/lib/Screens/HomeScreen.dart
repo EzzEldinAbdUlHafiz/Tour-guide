f44d673ef2fce6b00d44bf3b552538159a19df67
EzzEldinAbdulHafiz <ezzeldin2047@gmail.com>
2023-05-07 21:34:21 +0300

    add mqtt

--- client_tablet/lib/Screens/HomeScreen.dart
+++ client_tablet/lib/Screens/HomeScreen.dart
@@ -1,9 +1,8 @@
 import 'package:client_tablet/Globals.dart';
 import 'package:client_tablet/Screens/VideoScreen.dart';
+import 'package:client_tablet/Screens/welcomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:mqtt_client/mqtt_client.dart';
-import 'package:mqtt_client/mqtt_server_client.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,22 +12,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final client = MqttServerClient(Globals().mqtt, 'client1');
-
-  Future<void> mqttConnect() async {
-    client.keepAlivePeriod = 20;
-    await client.connect();
-    final builder = MqttClientPayloadBuilder();
-    builder.addString('Hello, dart!');
-    client.publishMessage('my/topic', MqttQos.atLeastOnce, builder.payload!);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    mqttConnect();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -77,7 +60,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const VideoScreen()),
+                        builder: (context) => const WelcomeScreen()),
                   );
                   // Navigate to the next screen or window
                 },
---
+++ client_tablet/lib/Screens/welcomeScreen.dart
@@ -0,0 +1,77 @@
+// ignore_for_file: library_private_types_in_public_api
+import 'package:client_tablet/Screens/TransitionScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  double h = 0, w = 0, topPadding = 0;
+  late VideoPlayerController _controller;
+
+  void checkVideo() {
+    if (_controller.value.position ==
+        const Duration(seconds: 0, minutes: 0, hours: 0)) {
+      debugPrint(
+          'video Started...........................................................................');
+    }
+
+    if (_controller.value.position == _controller.value.duration) {
+      debugPrint(
+          'video Ended.............................................................................');
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const TransitionScreen()),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(
+      'assets/videos/vid.mp4',
+    )..initialize().then((_) {
+        setState(() {
+          _controller.play();
+        });
+      });
+    _controller.addListener(checkVideo);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    h = MediaQuery.of(context).size.height;
+    w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            if (_controller.value.isInitialized)
+              SizedBox(
+                width: w,
+                height: h,
+                child: AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                ),
+              )
+            else
+              Container(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
